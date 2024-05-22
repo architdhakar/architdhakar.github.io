0f5ae3d8e4c84c54cc16f5393a47538863358012
Archit Dhakar <117572754+architdhakar@users.noreply.github.com>
2024-05-22 13:40:11 +0530

    script.js

--- script.js
+++ script.js
@@ -2,3 +2,32 @@ function toggleMenu() {
     var nav = document.querySelector('nav ul');
     nav.classList.toggle('show');
 }
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleSwitch = document.getElementById('toggleSwitch');
+    const body = document.body;
+    const navbar = document.querySelector('.navbar');
+
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        body.classList.add(savedTheme);
+        navbar.classList.add(savedTheme === 'dark-mode' ? 'navbar-dark' : 'navbar-light');
+        toggleSwitch.checked = savedTheme === 'dark-mode';
+    } else {
+        body.classList.add('light-mode');
+        navbar.classList.add('navbar-light');
+    }
+
+    toggleSwitch.addEventListener('change', () => {
+        if (toggleSwitch.checked) {
+            body.classList.replace('light-mode', 'dark-mode');
+            navbar.classList.replace('navbar-light', 'navbar-dark');
+            localStorage.setItem('theme', 'dark-mode');
+        } else {
+            body.classList.replace('dark-mode', 'light-mode');
+            navbar.classList.replace('navbar-dark', 'navbar-light');
+            localStorage.setItem('theme', 'light-mode');
+        }
+    });
+});
